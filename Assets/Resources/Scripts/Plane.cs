c843e3f5969514f905494295c62c5210bb38bef8
sp4ceforc3 <85077306+sp4ceforc3@users.noreply.github.com>
2023-05-13 11:02:45 +0200

    Player Controller - fuel
    - limited boost
    - praise the sun

--- Assets/Resources/Scripts/Plane.cs
+++ Assets/Resources/Scripts/Plane.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class Plane : MonoBehaviour
 {
@@ -10,9 +12,12 @@ public class Plane : MonoBehaviour
     [SerializeField] float animationTime = 1f;
     [SerializeField] Sprite[] moveSprites;
     [SerializeField] AudioSource audioSource;
+    [SerializeField] TextMeshProUGUI fuelText;
+    [SerializeField] Image fuelBar;
     
     Rigidbody2D rigBody;
     SpriteRenderer sr;
+    float fuel = 1f;
 
     float timer;
 
@@ -23,7 +28,8 @@ public class Plane : MonoBehaviour
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
-        rigBody = GetComponent<Rigidbody2D>();  
+        rigBody = GetComponent<Rigidbody2D>();
+        fuelText.text = "100";
     }
 
     void fixedUpdate(){
@@ -33,10 +39,15 @@ public class Plane : MonoBehaviour
     void Update()
     {
         rigBody.velocity = new Vector2(speed, rigBody.velocity.y);
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && fuel > 0)
         {
+            fuel -= 0.2f * Time.deltaTime;
             audioSource.mute = false;
             rigBody.velocity = new Vector2(rigBody.velocity.x, jumpForce);
+            if (fuel <= 0)
+                fuelText.text = "Empty";
+            else 
+                fuelText.text = Mathf.Floor((fuel * 100)).ToString();
         }
             
         
